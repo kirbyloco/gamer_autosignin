24bbe09703065cee040f8414a56a6a6ed11fdfa0
耀風 <adwin82627459@gmail.com>
2019-11-04 08:57:49 +0800

    2019/11/04

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# gamer_autosignin
- 巴哈姆特自動簽到
+# 巴哈姆特自動簽到
+
+登入程式碼參考
+[XinBow99/BAHA_POSTER_Python](https://github.com/XinBow99/BAHA_POSTER_Python "XinBow99/BAHA_POSTER_Python")
+
+目前穩定性待測試
---
+++ main.py
@@ -0,0 +1,47 @@
+import requests
+import os
+from configobj import ConfigObj
+
+config = ConfigObj(os.getcwd()+"/config.conf")['Account']
+
+def _autosign(baseLogin):
+    token = baseLogin.get('https://www.gamer.com.tw/ajax/get_csrf_token.php').text
+    jsoninfo = baseLogin.post('https://www.gamer.com.tw/ajax/signin.php', data={'action': '1', 'token': token})
+    jsoninfo = jsoninfo.json()
+    print(token)
+    if jsoninfo['message'] == '簽到成功':
+        print('簽到成功')
+
+def _login(data):
+    baseLogin = requests.session()
+    baseLogin.headers.update(
+        {
+        'user-agent': 'Bahadroid (https://www.gamer.com.tw/)',
+        'x-bahamut-app-instanceid': 'cc2zQIfDpg4',
+        'x-bahamut-app-android': 'tw.com.gamer.android.activecenter',
+        'x-bahamut-app-version': '251',
+        'content-type': 'application/x-www-form-urlencoded',
+        'content-length': '44',
+        'accept-encoding': 'gzip',
+        'cookie': 'ckAPP_VCODE=7045'
+        },
+    )
+
+    account = baseLogin.post('https://api.gamer.com.tw/mobile_app/user/v3/do_login.php', data=data)
+    account_f = account.json()
+    baseLogin.headers = {
+        'user-agent': 'Bahadroid (https://www.gamer.com.tw/)',
+        'x-bahamut-app-instanceid': 'cc2zQIfDpg4',
+        'x-bahamut-app-android': 'tw.com.gamer.android.activecenter',
+        'x-bahamut-app-version': '251',
+        'accept-encoding': 'gzip'
+    }
+    _autosign(baseLogin)
+
+if __name__ == "__main__":
+    data = {
+        'uid': config['UID'], 
+        'passwd': config['PASSWD'],
+        'vcode': '7045'
+    }
+    _login(data)
